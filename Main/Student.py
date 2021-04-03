6e9845a3a6ae3cfa2627acb4f93933e35b88428c
HongDavid8620 <davidhong8620@gmail.com>
2021-04-03 09:25:17 +0700

    vid

---
+++ Main/Student.py
@@ -0,0 +1,11 @@
+from Classes.People import *
+
+class Student(People):
+    subject=None
+    year=None
+    def input(self):
+        super().input()
+        self.subject = str(input("Enter subject= "))
+        self.year = int(input("Enter year= "))
+    def output(self):
+        return super().output() + '{0:<10}'.format(self.subject) + str(self.year)
